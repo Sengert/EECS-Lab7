1920271715346f40dfacc5009a5427ef390d39a1
Sengert <maynard-2006@hotmail.com>
2017-03-27 11:59:26 -0500

    Converted determinant()
    Converted the determinant() function that was given in C++ to Java

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -13,7 +13,38 @@ public class Matrix {
 		
 	}
 	
+	//--------------
+	//Functions
+	//--------------
 	
+	public Matrix(int size){
+		n = size;
+	}
 	
+	public double determinant(){
+		double det = 0.0;
+		
+		if(n == 1){
+			det = mdata[0][0];
+		}
+		else if(n == 2){
+			det = (mdata[0][0] * mdata[1][1]) - (mdata[0][1] * mdata[1][0]);
+		}
+		else{
+			for(int i = 0; i < n; i++){
+				det += Math.pow(-1.0, (double)i) * (mdata[0][i] * subMatrix(0,i).determinant());
+			}
+		}
+		
+		return det;
+	}
+	
+	Matrix inverse(){
+		
+	}
+	
+	Matrix subMatrix(int row, int col){
+		
+	}
 	
 }
