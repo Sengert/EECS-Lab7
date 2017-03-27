bc14bb64891783be46363e3efd68a2c134ae3d45
Sengert <maynard-2006@hotmail.com>
2017-03-27 11:51:44 -0500

    Added in Matrix.java to the repository
    Testing adding files to the repository

---
+++ src/Matrix/Matrix.java
@@ -0,0 +1,19 @@
+package Matrix;
+import java.lang.Math;
+
+public class Matrix {
+	
+	public int MaxSize = 10;
+	
+	private int n;
+	private double mdata[][] = new double[MaxSize][MaxSize];
+	
+	
+	public Matrix(){
+		
+	}
+	
+	
+	
+	
+}
